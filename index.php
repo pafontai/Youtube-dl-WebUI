5d9a978fe0085bc0596a32698739619f0a6c425e
Uli Koeth <uli@deepthought.kiot.eu>
2017-03-16 22:29:19 +0100

    Rename column on music list

--- index.php
+++ index.php
@@ -388,7 +388,7 @@
           <tr>
             <th style="min-width:800px; height:35px">File</th>
             <th style="min-width:80px">Size</th>
-            <th style="min-width:110px">Delete link</th>
+            <th style="min-width:110px">Actions</th>
           </tr>
         </thead>
         <tbody>
