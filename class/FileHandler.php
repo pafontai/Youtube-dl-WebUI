a4aa265f02a2ab6eaaf2bf064e1a63028a4a0437
DavidTre07 <david.tre07@gmail.com>
2017-04-29 22:43:31 +0200

    Updated to support Alpine linux (removed the use of glob function)

--- class/FileHandler.php
+++ class/FileHandler.php
@@ -2,8 +2,8 @@
 
 class FileHandler
 {
-    private $videos_ext = ".{avi,mp4,flv,webm,3gp}";
-    private $musics_ext = ".{mp3,ogg,m4a}";
+    private $videos_ext = "avi|mp4|flv|webm|3gp";
+    private $musics_ext = "mp3|ogg|m4a";
     private $config = [];
 
     public function __construct()
@@ -21,14 +21,18 @@ class FileHandler
 
         $folder = $this->get_downloads_folder().'/';
 
-        foreach(glob($folder.'*'.$this->videos_ext, GLOB_BRACE) as $file)
-        {
-            $video = [];
-            $video["name"] = str_replace($folder, "", $file);
-            $video["size"] = $this->to_human_filesize(filesize($file));
-            
-            $videos[] = $video;
+        $dir_handle=opendir($folder);
+        while ($file = readdir($dir_handle)) {
+            if ($file != "." && $file != "..") {
+                if(preg_match('/^.*\.('.$this->videos_ext.')$/i',$file)) {
+                    $video = [];
+                    $video["name"] = str_replace($folder, "", $file);
+                    $video["size"] = $this->to_human_filesize(filesize($folder.$file));
+                    $videos[] = $video;
+                }
+            }
         }
+        closedir($dir_handle);
 
         return $videos;
     }
@@ -43,13 +47,28 @@ class FileHandler
 
         $folder = $this->get_downloads_folder().'/';
 
+<<<<<<< HEAD
+        $dir_handle=opendir($folder);
+        while ($file = readdir($dir_handle)) {
+            if ($file != "." && $file != "..") {
+                if(preg_match('/^.*\.('.$this->musics_ext.')$/i',$file)) {
+                    $music= [];
+                    //$music["name"] = str_replace($folder, "", $file);
+                    $music["name"] = $file;
+                    $music["size"] = $this->to_human_filesize(filesize($folder.$file));
+                    $musics[] = $music;
+                }
+            }
+=======
         foreach(glob($folder.'*'.$this->musics_ext, GLOB_BRACE) as $file) {
             $music = [];
             $music["name"] = str_replace($folder, "", $file);
             $music["size"] = $this->to_human_filesize(filesize($file));
             
             $musics[] = $music;
+>>>>>>> f9f1ef60f04fd1ab4c31efab45491c64483d22ce
         }
+        closedir($dir_handle);
 
         return $musics;
     }
